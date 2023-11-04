4de5429d09186c9b25ace8f79e98b604a5db17c6
Reinhardt <reinhardtcollins@live.com>
2023-11-04 16:40:59 +0300

    Progress

--- index.js
+++ index.js
@@ -1,17 +1,67 @@
 let exp = '0'
 
+let a = ''
+let b = ''
+let operator = ''
+
+let special = ['%', '/', 'x', '-', '+', '=', 'C', '.']
+
 let buttons = document.querySelectorAll('.input-col')
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function() {
         let input = buttons[i].innerHTML
 
-        if (exp === '0') {
-            exp = input
-        } else if (input === "C") {
-            exp = "0"
+        if (special.includes(input)) {
+            if (input === 'C') {
+                a = ''
+                b = ''
+                operator = ''
+
+                exp = '0'
+            } else if (input === '=') {
+                let x = parseFloat(a)
+                let y = parseFloat(b)
+                
+                if (operator === '+') {
+                    exp = x + y
+                } else if (operator === '-') {
+                    exp = x - y
+                } else if (operator === 'x') {
+                    exp = x * y
+                } else if (operator === '/') {
+                    exp = x / y
+                } else if (operator === '%') {
+                    exp = x % y
+                }
+
+                a = exp
+                b = ''
+                operator = ''
+            } else if (input === '.') {
+                if (operator === '') {
+                    if (!a.includes('.')) {
+                        a = a + input
+                    }
+                } else {
+                    if (!b.includes('.')) {
+                        b = b + input
+                    }
+                }
+
+                exp = a + operator + b
+            } else {
+                operator = input
+                exp = a + operator + b
+            }
         } else {
-            exp = exp + input
+            if (operator === '') {
+                a = a + input
+            } else {
+                b = b + input
+            }
+
+            exp = a + operator + b
         }
         
         document.querySelector('.display-text').innerHTML = exp
